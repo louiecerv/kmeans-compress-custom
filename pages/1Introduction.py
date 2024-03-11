93cd0fe3fe8cb45fbedcc2159735f9d4b3f68242
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-12 01:41:36 +0800

    Save

--- pages/1Introduction.py
+++ pages/1Introduction.py
@@ -12,10 +12,10 @@ def app():
     st.subheader('Upload an image for the compression task.')
     uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png"])
     if uploaded_file is not None:
-    # Convert uploaded file to bytes
-    image_bytes = uploaded_file.read()
-    # Display uploaded image
-    st.image(image_bytes, caption="Uploaded Image")
+        # Convert uploaded file to bytes
+        image_bytes = uploaded_file.read()
+        # Display uploaded image
+        st.image(image_bytes, caption="Uploaded Image")
 
 
 #run the app
