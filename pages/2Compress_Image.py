15314e64060a4030be5f8dda1c43aae1a9db21b3
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-21 01:26:13 +0000

    Save

--- pages/2Compress_Image.py
+++ pages/2Compress_Image.py
@@ -12,16 +12,18 @@ def app():
     data = st.session_state.image_data
 
     original = st.session_state.original
+    original_data = st.session_state.original_data
+    normalized_data = st.session_state.normalized_data
 
     kmeans = MiniBatchKMeans(16)
-    kmeans.fit(data)
+    kmeans.fit(normalized_data)
 
-    new_colors = kmeans.cluster_centers_[kmeans.predict(data)]
+    new_colors = kmeans.cluster_centers_[kmeans.predict(normalized_data)]
 
     #plot_pixels(data, 'Reduced color space: 16 colors', new_colors)
 
     #convert the array to an image
-    img_recolored = new_colors.reshape(original.shape)
+    img_recolored = new_colors.reshape(normalized_data.shape)
 
     fig, ax = plt.subplots(1, 2, figsize=(16,6), subplot_kw=dict(xticks=[], yticks=[]))
     fig.subplots_adjust(wspace=0.05)
